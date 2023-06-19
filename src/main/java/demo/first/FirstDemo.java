b5f6b11f43f70139d5ed9b5b15a6cb970cf21df5
Ibraim Asanov <iasanov@nemetschek.bg>
2023-06-19 20:38:21 +0300

    Initial commit

---
+++ src/main/java/demo/first/FirstDemo.java
@@ -0,0 +1,11 @@
+//definition of package
+package demo.first;
+
+//definition of a class
+public class FirstDemo {
+
+    //method
+    public static void main (String [] args){
+        System.out.println("Hello JAVA");
+    }
+}
